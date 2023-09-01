855df306cbcfa37a16eae0afd38c5319c29f3ef2
Logan Belews <Loganbelew2@gmail.com>
2023-09-01 14:49:46 -0500

    top agent table

---
+++ top_agent.sql
@@ -0,0 +1,10 @@
+SELECT SalesRep, MAX(TotalSales) AS Sales FROM (
+SELECT e.FirstName || ' ' || e.LastName AS SalesRep,
+SUM(i.Total) AS TotalSales
+FROM Customer c 
+JOIN Invoice i 
+ON c.CustomerId = i.CustomerId
+JOIN Employee e 
+ON e.EmployeeId = c.SupportRepId
+GROUP BY SalesRep
+)
